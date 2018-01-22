6889d7c40eedc0b88009ca1670f0707b56df0ff5
Khinenw <deu07115@gmail.com>
2018-01-22 23:24:55 +0900

    Updated for windows, (changed to electron-vibrancy)

--- index.js
+++ index.js
@@ -15,11 +15,14 @@ function makeTransparent(color, alpha = DEFAULT_ALPHA) {
   return `rgba(${rgb.join(', ')}, ${alpha})`;
 }
 
-module.exports.onWindow = browserWindow => vibrancy.setVibrancy(browserWindow, 2);
+module.exports.onWindow = browserWindow => {
+	vibrancy.SetVibrancy(browserWindow, 2);
+};
 
 module.exports.decorateConfig = config => {
   const { alpha } = config[CONFIG_KEY] || {};
   return Object.assign({}, config, {
     backgroundColor: makeTransparent(config.backgroundColor, alpha),
+	transparent: true
   });
 };
